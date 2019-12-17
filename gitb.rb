ca7fa89da6d865d0f6c4ad4fcebfe94ba4fda0a1
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2019-12-18 06:57:36 +0900

    update version 2.2.1

--- gitb.rb
+++ gitb.rb
@@ -2,13 +2,13 @@ class Gitb < Formula
     desc "A command line tool for using Backlog's git comfortably"
     homepage "https://github.com/vvatanabe/gitb"
     if Hardware::CPU.is_64_bit?
-        url "https://github.com/vvatanabe/gitb/releases/download/2.2.0/gitb_2.2.0_darwin_amd64.zip"
-        sha256 "69c2081ed3184ab9b66f27dbc8a9f67e711778b802911ddb339a0cfa1b1265af"
+        url "https://github.com/vvatanabe/gitb/releases/download/2.2.1/gitb_2.2.1_darwin_amd64.zip"
+        sha256 "b03a44dd80854aec96f928787dd9de9b46b9bbf2d8bf9f1963f8f98cadcd776b"
     else
-        url "https://github.com/vvatanabe/gitb/releases/download/2.2.0/gitb_2.2.0_darwin_386.zip"
-        sha256 "9f5c5c9e24a93132ccbeea53e20f3f63e09efa777b68bbf622f8116de0f24035"
+        url "https://github.com/vvatanabe/gitb/releases/download/2.2.1/gitb_2.2.1_darwin_386.zip"
+        sha256 "7b52348bf0f436db32547570379da8a456f6f4a63d8bae0ca43748ef65cd2deb"
     end
-    version "2.2.0"
+    version "2.2.1"
     def install
         bin.install 'gitb'
     end
